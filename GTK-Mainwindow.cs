0ddf516007ab6e5b2e4e074d66dae99e900160e0
skounah <el_skoner@hotmail.com>
2015-01-21 09:42:12 +0100

    Create GTK-Mainwindow.cs

---
+++ GTK-Mainwindow.cs
@@ -0,0 +1,24 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButtonAceptarClicked (object sender, EventArgs e)
+	{
+		labelSaludo.Text = " Hola " + entryNombre.Text;
+	}
+
+		
+}
+
